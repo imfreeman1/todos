79659da2778b9a2b9e5eb37c67af833c28ba3401
imfreeman1 <dkfrhkt@gmail.com>
2023-05-22 19:19:56 +0900

    TodoContainer 기능 추가
     - handleSubmit 기능 제작
     - checkBox 추가 예정
     - removeHandler 추가 예정

--- src/Components/Todos/TodoContainer.tsx
+++ src/Components/Todos/TodoContainer.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { MouseEvent, useRef } from 'react';
 import TodoPresent from './TodoPresent';
 import App from 'next/app';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from 'src/Redux/Store';
 import { setTodo } from 'src/Redux/action';
+import { v4 } from 'uuid';
 
 // interface는 객체를 만들 때, 사용하는 것 같음.
 
 const TodoContainer: React.FC = () => {
 	const todoList = useSelector((state: RootState) => state.todo);
+	console.log(todoList);
+	const inputRef = useRef<HTMLInputElement>(null);
 	const dispatch = useDispatch();
 
-	const handleSubmit = (event: Event) => {
-		console.log(event);
-		dispatch(setTodo({ id }));
+	const removeHandler = () => {};
+
+	const handleSubmit = (event: MouseEvent) => {
+		event.preventDefault();
+		const newID: string = v4();
+		if (inputRef.current && inputRef.current.value !== '') {
+			dispatch(setTodo({ id: newID, content: inputRef.current.value }));
+			inputRef.current.value = '';
+		}
 	};
 	return (
 		<TodoPresent
 			todoList={todoList}
 			handleSubmit={handleSubmit}
+			inputRef={inputRef}
+			onClick={removeHandler}
 		/>
 	);
 };
