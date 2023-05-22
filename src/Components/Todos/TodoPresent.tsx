db64d885e24ce52c19817733587f8f7b119b6cce
imfreeman1 <dkfrhkt@gmail.com>
2023-05-22 19:21:22 +0900

    TodoPresent 수정
     - CSS 대거 수정
     - ITodo의 id type 변경
     - Props의 inputRef, onClick 추가

--- src/Components/Todos/TodoPresent.tsx
+++ src/Components/Todos/TodoPresent.tsx
@@ -1,35 +1,49 @@
-import React from 'react';
+import React, { MouseEvent, RefObject } from 'react';
+import Button from '../Button/Button';
 
 interface ITodo {
-	id: number;
+	id: string;
 	content: string;
 }
 
 interface Props {
 	todoList: ITodo[];
 	handleSubmit(event: MouseEvent): void;
+	inputRef: RefObject<HTMLInputElement>;
+	onClick(): void;
 }
 
-const TodoPresent = ({ todoList, handleSubmit }: Props) => {
+const TodoPresent = ({ todoList, handleSubmit, inputRef, onClick }: Props) => {
 	return (
-		<section className="grid grid-cols-8 grid-rows-6 w-screen h-screen">
-			<div className=" col-start-3 col-span-4 row-start-2 row-span-4 bg-blue-400 flex flex-col items-center">
+		<section className="grid grid-cols-8 grid-rows-8 w-screen h-screen">
+			<div className=" col-start-3 col-span-4 row-start-2 row-span-6 bg-orange-200 flex flex-col items-center rounded-lg shadow-xl">
 				<div className="my-6">
 					<h1 className="font-bold text-6xl">TodoList</h1>
 				</div>
-				<div>
+				<div className=" w-2/3 border-2 border-black rounded-lg p-3 grow">
 					<ul>
 						{todoList.map((todo, idx) => (
-							<li key={idx}>
-								<div>{todo.content}</div>
+							<li
+								className="flex my-2 gap-2 justify-start"
+								key={idx}
+							>
+								<input type="checkbox" />
+								<span className="grow">{todo.content}</span>
+								<Button
+									className=""
+									content="삭제"
+									onClick={onClick}
+									type="button"
+								/>
 							</li>
 						))}
 					</ul>
 				</div>
-				<form>
+				<form className="my-4 gap-2 flex">
 					<input
 						className=" placeholder:bg-white border-2 border-black rounded-md p-1"
 						placeholder="내용"
+						ref={inputRef}
 					/>
 					<input
 						type="submit"
